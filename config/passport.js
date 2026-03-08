e8c6e9c3d478aee6f337bd2752dd28a00e0b25da
ashmi-m <ashmim617@gmail.com>
2026-03-09 02:01:42 +0530

    cloudinary

--- config/passport.js
+++ config/passport.js
@@ -8,20 +8,15 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/callback"
-
 },
    async (accessToken, refreshToken, profile, done) => {
-    // console.log("google profile",profile);
-    
     try {
         let user = await User.findOne({ googleId: profile.id });
         if (user) {
             console.log("user found",user);
-            
             return done(null, user);
         } else {
             console.log("creating user");
-            
             user = new User({
                 name: profile.displayName,
                 email: profile.emails[0].value,
@@ -32,11 +27,9 @@ passport.use(new GoogleStrategy({
         }
     } catch (err) {
         console.log("error happened in google passport",err);
-        
         return done(err, null);
     }
 }));
-
 passport.serializeUser((user,done)=>{
     done(null,user.id)
 });
@@ -49,5 +42,4 @@ passport.deserializeUser((id,done)=>{
         done(err,null)
     })
 })
-
 module.exports=passport;
--- config/razorpay.js
+++ config/razorpay.js
@@ -6,7 +6,6 @@ const razorpay = new Razorpay({
   key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
-
 module.exports = razorpay;
 
 
